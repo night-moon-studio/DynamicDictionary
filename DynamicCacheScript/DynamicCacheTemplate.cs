022701d67d0683f6dc9dfd137ffe0a454ef80e90
NMSAzulX <16236903+NMSAzulX@users.noreply.github.com>
2019-10-30 14:36:07 +0800

    上传项目

---
+++ DynamicCacheScript/DynamicCacheTemplate.cs
@@ -0,0 +1,17 @@
+﻿using Natasha;
+using System;
+using System.Text;
+namespace DynamicCacheScript
+{
+    public class DynamicCacheTemplate
+    {
+        public static StringBuilder GetFromStaticClass(Type operatorType, string operatorMethod, Type builderType, string builderMethod,  Func<string,string> builderParameter = null)
+        {
+            StringBuilder builder = new StringBuilder();
+            string operatorTypeName = operatorType.GetDevelopName();
+            builder.Append($"{operatorTypeName}.{operatorMethod} = {builderType.GetDevelopName()}.{builderMethod}({builderParameter?.Invoke("arg")??"arg"});");
+            builder.Append($"return {operatorTypeName}.{operatorMethod}(arg);");
+            return builder;
+        }
+    }
+}
