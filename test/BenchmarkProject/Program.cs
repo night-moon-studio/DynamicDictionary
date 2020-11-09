f3a70e9c70eadf19eb0078ede3819cec3f0cac1d
AzulX <16236903+NMSAzulX@users.noreply.github.com>
2020-11-09 09:34:46 +0800

    Revert "修复UT测试"
    This reverts commit 2a88abf0f616c6001cf284861d1b20a92d5699b1.

--- test/BenchmarkProject/Program.cs
+++ test/BenchmarkProject/Program.cs
@@ -8,7 +8,7 @@ namespace BenchmarkProject
     {
         static void Main(string[] args)
         {
-           
+            DomainManagement.RegisterDefault<AssemblyDomain>();
             BenchmarkRunner.Run<BenchmarkTest>();
             Console.ReadKey();
         }
